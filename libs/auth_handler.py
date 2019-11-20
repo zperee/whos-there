4d0dbbd47fd0f6ffd0fd1e94e0cf16bfba5421c9
zperee <elia.perenzin@bluewin.ch>
2019-11-20 10:26:03 +0100

    check if stored_password fixes #8

--- libs/auth_handler.py
+++ libs/auth_handler.py
@@ -55,11 +55,14 @@ def hash_password(password):
 
 def verify_password(stored_password, provided_password):
     """Verify a stored password against one provided by user"""
-    salt = stored_password[:64]
-    stored_password = stored_password[64:]
-    pwdhash = hashlib.pbkdf2_hmac('sha512', 
-                                  provided_password.encode('utf-8'), 
-                                  salt.encode('ascii'), 
-                                  100000)
-    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
-    return pwdhash == stored_password
+    pwdhash = ""
+    if stored_password:
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                                    provided_password.encode('utf-8'), 
+                                    salt.encode('ascii'), 
+                                    100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        pwdhash == stored_password
+    return pwdhash 
